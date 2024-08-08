d5ebac05beca28357c1a54a55913cae18efd0034
Richard Ma <richard.ma.19850509@gmail.com>
2024-08-08 16:38:36 +0800

    add get_id() & check_password()

--- user.py
+++ user.py
@@ -7,3 +7,9 @@ from database import db
 class User(db.Model):
     id: Mapped[str] = mapped_column(primary_key=True)
     passwrod: Mapped[str] = mapped_column(nullable=False)
+
+    def get_id(self):
+        return self.id
+
+    def check_password(self, password):
+        return self.passwrod == password
